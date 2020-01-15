698f4950237b1272a31d6840f8dfab24f12b5b75
Laurent Verbeke <laurent.verbeke@gmail.com>
2020-01-15 11:18:21 +0100

    comit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,3 @@
-//@prepros-append libs/primary/jquery.js
 //@prepros-append libs/secondary/jquery.easing.1.3.js
 //@prepros-append libs/secondary/jquery.sticky-kit.min.js
 //@prepros-append libs/secondary/scrpit-chosen.jquery.js
